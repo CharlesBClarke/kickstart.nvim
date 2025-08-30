b2c1569b824e5aead7fb0b86c604d881105feb10
charles clarke <charles_b_clarke@outlook.com>
2025-08-29 21:41:10 -0700

    alpha kinda works

--- lua/Smappings/defs.lua
+++ lua/Smappings/defs.lua
@@ -1,13 +1,42 @@
 return function(mm)
-  -- Colemak movement
+  -- Colemak (mode = '')
   mm.add('colemak', '', 'n', 'j')
+  mm.add('colemak', '', 'N', 'J')
   mm.add('colemak', '', 'e', 'k')
+  mm.add('colemak', '', 'E', 'K')
   mm.add('colemak', '', 'i', 'l')
+  mm.add('colemak', '', 'I', 'L')
+  mm.add('colemak', '', 'm', 'h')
   mm.add('colemak', '', 'M', 'H')
 
-  -- QWERTY restore
-  mm.add('qwerty', '', 'h', 'h')
-  mm.add('qwerty', '', 'j', 'j')
-  mm.add('qwerty', '', 'k', 'k')
-  mm.add('qwerty', '', 'l', 'l')
+  mm.add('colemak', '', 'j', 'm')
+  mm.add('colemak', '', 'J', 'M')
+  mm.add('colemak', '', 'k', 'n')
+  mm.add('colemak', '', 'K', 'N')
+  mm.add('colemak', '', 't', 'i')
+  mm.add('colemak', '', 'T', 'I')
+  mm.add('colemak', '', 'h', 'e')
+  mm.add('colemak', '', 'H', 'E')
+  mm.add('colemak', '', 'l', 't')
+  mm.add('colemak', '', 'L', 'T')
+
+  -- windows
+  mm.add('colemak', '', '<C-w>n', '<C-w>j')
+  mm.add('colemak', '', '<C-w>e', '<C-w>k')
+  mm.add('colemak', '', '<C-w>i', '<C-w>l')
+  mm.add('colemak', '', '<C-w>m', '<C-w>h')
+
+  -- resize
+  mm.add('colemak', '', '<C-w>J', '<C-w>-')
+  mm.add('colemak', '', '<C-w>E', '<C-w>+')
+  mm.add('colemak', '', '<C-w>I', '<C-w>>')
+  mm.add('colemak', '', '<C-w>M', '<C-w><')
+
+  -- swap
+  mm.add('colemak', '', '<C-w><C-n>', '<C-w>K')
+  mm.add('colemak', '', '<C-w><C-e>', '<C-w>J')
+  mm.add('colemak', '', '<C-w><C-i>', '<C-w>L')
+  mm.add('colemak', '', '<C-w><C-m>', '<C-w>H')
+
+  -- QWERTY: keep empty; switching clears these.
 end
